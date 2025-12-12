92c1a44bbc55fd277acfc796c804d4bc4e052bd7
rnozila2197 <yjamshid2197@gmail.com>
2025-12-12 16:06:41 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1
+import math
+
+print(math.sqrt(144))
+print(math.sqrt(225))
+print(math.sqrt(169))
+print(math.sqrt(81))
+print(math.sqrt(400))
+print(math.sqrt(121))
+print(math.sqrt(49))
+print(math.sqrt(625))
